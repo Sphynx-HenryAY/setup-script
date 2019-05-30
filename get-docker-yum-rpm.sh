aa228584fd0dd814c65b3ebb13bc25238ce227c2
tms <tms@git.lab.tms.local>
2019-05-30 15:49:55 +0800

    Add current user to docker group and start docker as startup

--- get-docker-yum-rpm.sh
+++ get-docker-yum-rpm.sh
@@ -2,3 +2,5 @@ sudo yum install wget
 wget https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-18.03.1.ce-1.el7.centos.x86_64.rpm
 sudo yum install docker-ce-18.03.1.ce-1.el7.centos.x86_64.rpm -y
 rm docker-ce-18.03.1.ce-1.el7.centos.x86_64.rpm
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
